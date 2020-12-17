08e6292a3a7a6db1aa8e355afef375139dd24838
chandanhr-7 <76148975+chandanhr-7@users.noreply.github.com>
2020-12-17 13:51:32 +0530

    added diff method

--- Calculator.java
+++ Calculator.java
@@ -1,10 +1,15 @@
 class Calculator{
     public static int add(int num1,int num2){
-      return num1=num2;
+     return num1+num2;
+    } 
+    
+    public static int diff(int num1,int num2){
+     return num1-num2;
     }
     
     public static void main(String []args){
       System.out.println(add(10,20));
+      System.out.println(diff(10,20));
     }
 
 }
